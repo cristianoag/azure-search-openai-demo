fa00f2d7cf9e63ab6297bcedb864d5b2b87addf2
cristianoag <cristianoag@hotmail.com>
2026-03-09 17:56:07 -0300

    Coke

--- app/frontend/src/components/QuestionInput/QuestionInput.tsx
+++ app/frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -114,12 +114,7 @@ export const QuestionInput = ({ onSend, onStop, disabled, placeholder, clearOnSe
                     </Tooltip>
                 ) : (
                     <Tooltip content={t("tooltips.submitQuestion")} relationship="label">
-                        <Button
-                            size="large"
-                            icon={<Send28Filled primaryFill="#B2172F" />}
-                            disabled={sendQuestionDisabled}
-                            onClick={sendQuestion}
-                        />
+                        <Button size="large" icon={<Send28Filled primaryFill="#B2172F" />} disabled={sendQuestionDisabled} onClick={sendQuestion} />
                     </Tooltip>
                 )}
             </div>
